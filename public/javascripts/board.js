0ecdcaf8c577703f05bcc3f20d48f57d65cf2b0c
sungjine456 <sungjine456@gmail.com>
2017-09-06 17:47:45 +0900

    Rest API의 규칙에 맞는 URI로 수정

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -2,7 +2,7 @@ angular.module('boardApp', [])
     .controller('BoardListController', function ($scope, $http) {
         $http({
             method: 'GET',
-            url: '/getBoard'
+            url: '/board-titles'
         }).then(function successCallback(response) {
             $scope.titles = response.data
         }, function errorCallback(response) {
