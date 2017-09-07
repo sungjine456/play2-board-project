d1970986c3b2a4b5929fc0be6d615fe5e61fd908
sungjine456 <sungjine456@gmail.com>
2017-09-07 10:47:54 +0900

    $http 대신 $resource 사용

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,11 +1,4 @@
-angular.module('boardApp', [])
-    .controller('BoardListController', function ($scope, $http) {
-        $http({
-            method: 'GET',
-            url: '/board-titles'
-        }).then(function successCallback(response) {
-            $scope.titles = response.data
-        }, function errorCallback(response) {
-            alert(response.status);
-        });
+angular.module('boardApp', ['ngResource'])
+    .controller('BoardListController', function ($scope, $resource) {
+        $scope.titles = $resource('/board-titles').query();
     });
