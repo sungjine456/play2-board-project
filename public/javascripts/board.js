7e1e117dfc06fd0adb70027ed8be0f4a774f9edd
sungjine456 <sungjine456@gmail.com>
2017-09-12 18:44:41 +0900

    directive 적용

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,11 +1,22 @@
 angular.module('boardApp', ['ngResource'])
-    .factory('boardService', function($resource) {
-        return $resource('/boards',{},{
-            query:{method:'GET', isArray:false}
+    .factory('boardService', function ($resource) {
+        return $resource('/boards', {}, {
+            query: {method: 'GET', isArray: false}
         });
     })
-    .controller('BoardListController', function ($scope, boardService) {
-        boardService.get(function(data) {
+    .controller('boardController', function ($scope, boardService) {
+        boardService.get(function (data) {
             $scope.boards = data.boards;
         });
+    })
+    .directive('boardList', function () {
+        return {
+            restrict: 'E',
+            require: false,
+            template: '<li ng-repeat="board in boards"> <div class="row">' +
+            '<div class="col-sm-1 text-center">{{$index + 1}}</div>' +
+            '<div class="col-sm-8">{{board.title}}</div>' +
+            '<div class="col-sm-3 text-center">{{board.writer}}</div>' +
+            '</div> </li>'
+        }
     });
