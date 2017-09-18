af8bdb222f945eb393c5db0445cc666d8aa3959f
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2017-09-18 22:08:05 +0900

    쿼리문의 일관성을 위한 수정

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,18 +3,18 @@
 -- 이름: 'Users'
 -- 설명: 사용자 정보를 관리하는 테이블.
 CREATE TABLE "Users" (
-  id        CHAR(16) PRIMARY KEY,
-  password  CHAR(16) NOT NULL,
-  name      CHAR(16) NOT NULL
+  "id"        CHAR(16) PRIMARY KEY,
+  "password" CHAR(16) NOT NULL,
+  "name"      CHAR(16) NOT NULL
 );
 
 -- 이름: 'Boards'
 -- 설명: 게시글 정보를 관리하는 테이블.
 CREATE TABLE "Boards" (
-  index    INTEGER PRIMARY KEY,
-  title    CHAR(100) NOT NULL,
-  context  CHAR(2000) NOT NULL,
-  writer   CHAR(16) NOT NULL
+  "index"    INTEGER PRIMARY KEY,
+  "title"    CHAR(100) NOT NULL,
+  "context"  CHAR(2000) NOT NULL,
+  "writer"   CHAR(16) NOT NULL
 );
 
 INSERT INTO "Users" VALUES('admin', '1234', 'admin');
