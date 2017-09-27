d652373c8cd410c0ebaf947e2cc9e82f16ca1ee2
sungjine456 <sungjine456@gmail.com>
2017-09-27 16:52:37 +0900

    코드 규칙을 적용하여 코드 정리

--- app/dao/BoardRepository.scala
+++ app/dao/BoardRepository.scala
@@ -8,9 +8,13 @@ import slick.jdbc.PostgresProfile.api._
 object BoardRepository {
   class Boards(tag: Tag) extends Table[Board](tag, "Boards") {
     def index = column[Long]("index", O.PrimaryKey)
+
     def title = column[String]("title")
+
     def context = column[String]("context")
+
     def writer = column[String]("writer")
+
     def * = (index, title, context, writer) <> (Board.tupled, Board.unapply _)
   }
 
