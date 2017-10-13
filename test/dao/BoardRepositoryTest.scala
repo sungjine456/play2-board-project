545e7773b61c2f48dc26acf0d77575ed9508645b
sungjine456 <sungjine456@gmail.com>
2017-10-13 16:51:48 +0900

    테스트의 정확도를 높이기 위한 테스트 데이터 추가와 테스트 코드 수정

--- test/dao/BoardRepositoryTest.scala
+++ test/dao/BoardRepositoryTest.scala
@@ -20,34 +20,39 @@ class BoardRepositoryTest extends AsyncFlatSpec with BeforeAndAfter with TestDat
 
   "BoardRepository.findAll()" should "find all boards" in {
     val result = repository.findAll()
-    val board = Some(Board(1, "title", "context", "admin"))
+    val headBoard = Some(Board(1, "title1", "context1", "admin"))
+    val lastBoard = Some(Board(4, "title4", "context4", "admin"))
 
     result map { boards =>
-      boards.size should equal(1)
-      boards.head should equal(board.value)
+      boards.size should equal(4)
+      boards.head should equal(headBoard.value)
+      boards.last should equal(lastBoard.value)
     }
   }
 
   "BoardRepository.delete(index)" should "delete a board that include the parameter index" in {
+    val resultBeforeDeletion = repository.findAll()
+
+    resultBeforeDeletion map { boards => boards.size should equal(4) }
+
     repository.delete(1L)
 
-    val result = repository.findAll()
+    val resultAfterDeletion = repository.findAll()
 
-    result map { boards =>
-      boards.size should equal(0)
-    }
+    resultAfterDeletion map { boards => boards.size should equal(3) }
   }
 
   "BoardRepository.update(board)" should "change a board with the parameter board" in {
-    val board = Board(1, "change title", "change context", "change admin")
+    val updateBoard = Board(1, "change title", "change context", "change admin")
 
-    repository.update(board)
+    val resultBeforeUpdating = repository.findAll()
 
-    val result = repository.findAll()
+    resultBeforeUpdating map { boards => boards.head should not equal updateBoard }
 
-    result map { boards =>
-      boards.size should equal(1)
-      boards.last should equal(board)
-    }
+    repository.update(updateBoard)
+
+    val resultAfterUpdating = repository.findAll()
+
+    resultAfterUpdating map { boards => boards.head should equal(updateBoard) }
   }
 }
--- test/resources/evolutions/default/1.sql
+++ test/resources/evolutions/default/1.sql
@@ -19,7 +19,10 @@ CREATE TABLE "Boards" (
 
 INSERT INTO "Users" VALUES('admin', '1234', 'admin');
 
-INSERT INTO "Boards" VALUES(1, 'title', 'context', 'admin');
+INSERT INTO "Boards" VALUES(1, 'title1', 'context1', 'admin');
+INSERT INTO "Boards" VALUES(2, 'title2', 'context2', 'admin');
+INSERT INTO "Boards" VALUES(3, 'title3', 'context3', 'admin');
+INSERT INTO "Boards" VALUES(4, 'title4', 'context4', 'admin');
 
 # --- !Downs
 
